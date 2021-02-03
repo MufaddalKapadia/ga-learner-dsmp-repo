bfb273eebee5078da309eccd19b9c0b10160e232
Mufaddal Kapadia <31702449+MufaddalKapadia@users.noreply.github.com>
2021-02-03 20:27:10 +0530

    Update code.py

--- Summarizing-Data-with-Statistics/code.py
+++ Summarizing-Data-with-Statistics/code.py
@@ -4,10 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 #path of the data file- path
-
-#Code starts here 
 
 #Reading of the file
 data=pd.read_csv(path)
@@ -22,9 +19,6 @@ gender_count=data['Gender'].value_counts()
 plt.bar(gender_count.index, gender_count)
 plt.show()
 
-
-# --------------
-#Code starts here
 #Storing the value count of 'Alignment' 
 alignment = data['Alignment'].value_counts()
 
@@ -37,10 +31,6 @@ plt.pie(alignment,labels=alignment.index,explode=(0.05,0.05,0.05),autopct='%1.1f
 #Setting the pie chart title
 plt.title('Character alignment')
 
-
-
-# --------------
-#Code starts here
 #Subsetting the data with columns ['Strength', 'Combat']
 sc_df = data[['Strength','Combat']].copy()
 
@@ -57,7 +47,6 @@ sc_combat = sc_df['Combat'].std()
 sc_pearson = sc_covariance/(sc_combat*sc_strength)
 
 print("Pearson's Correlation Coefficient between Strength and Combat : ", sc_pearson)
-
 
 #Subsetting the data with columns ['Intelligence', 'Combat']
 ic_df = data[['Intelligence','Combat']].copy()
@@ -76,9 +65,6 @@ ic_pearson = ic_covariance/(ic_intelligence*ic_combat)
 
 print("Pearson's Correlation Coefficient between Intelligence and Combat : ", ic_pearson)
 
-
-# --------------
-#Code starts here
 #Find the quantile=0.99 value of 'Total' column
 total_high= data['Total'].quantile(q=0.99)
 
@@ -91,9 +77,6 @@ super_best_names=list(super_best['Name'])
 #Printing the names
 print(super_best_names)
 
-
-# --------------
-#Code starts here
 #Setting up the subplots
 fig, (ax_1, ax_2,ax_3) = plt.subplots(1,3, figsize=(20,8))
 
@@ -103,20 +86,14 @@ ax_1.boxplot(super_best['Intelligence'])
 #Setting the subplot axis title
 ax_1.set(title='Intelligence')
 
-
 #Plotting box plot
 ax_2.boxplot(super_best['Speed'])
 
 #Setting the subplot axis title
 ax_2.set(title='Speed')
 
-
 #Plotting box plot
 ax_3.boxplot(super_best['Power'])
 
 #Setting the subplot axis title
 ax_3.set(title='Power')
-
-#Code ends here   
-
-
