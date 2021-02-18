7f95c75303cc547966ee78e4308bdf963927341f
Mufaddal Kapadia <31702449+MufaddalKapadia@users.noreply.github.com>
2021-02-18 21:44:13 +0530

    Update code.py

--- EDA-&-Data-Preprocessing/code.py
+++ EDA-&-Data-Preprocessing/code.py
@@ -3,16 +3,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 
-#Code starts here
 #Loading the data
 data=pd.read_csv(path)
 
 #Plotting histogram of Rating
 data['Rating'].plot(kind='hist')
-
 plt.show()
-
 
 #Subsetting the dataframe based on `Rating` column
 data=data[data['Rating']<=5]
@@ -20,11 +18,6 @@ data=data[data['Rating']<=5]
 #Plotting histogram of Rating
 data['Rating'].plot(kind='hist')  
 
-#Code ends here
-
-
-# --------------
-# code starts here
 #Sum of null values of each column
 total_null = data.isnull().sum()
 
@@ -33,7 +26,6 @@ percent_null = (total_null/data.isnull().count())
 
 #Concatenating total_null and percent_null values
 missing_data = pd.concat([total_null, percent_null], axis=1, keys=['Total', 'Percent'])
-
 print(missing_data)
 
 #Dropping the null values
@@ -47,15 +39,7 @@ percent_null_1 = (total_null_1/data.isnull().count())
 
 #Concatenating total_null and percent_null values
 missing_data_1 = pd.concat([total_null_1, percent_null_1], axis=1, keys=['Total', 'Percent'])
-
 print(missing_data_1)
-
-# code ends here
-
-
-# --------------
-
-#Code starts here
 
 #Setting the figure size
 plt.figure(figsize=(10,10))
@@ -68,15 +52,6 @@ cat.set_xticklabels(rotation=90)
 
 #Setting the title of the plot
 plt.title('Rating vs Category [BoxPlot]',size = 20)
-
-#Code ends here
-
-
-# --------------
-#Importing header files
-from sklearn.preprocessing import MinMaxScaler, LabelEncoder
-
-#Code starts here
 
 #Removing `,` from the column
 data['Installs']=data['Installs'].str.replace(',','')
@@ -102,14 +77,6 @@ sns.regplot(x="Installs", y="Rating", color = 'teal',data=data)
 #Setting the title of the plot
 plt.title('Rating vs Installs[RegPlot]',size = 20)
 
-
-#Code ends here
-
-
-
-# --------------
-#Code starts here
-
 data['Price'] = data['Price'].str.replace('$','')
 
 #Converting the column to float
@@ -124,12 +91,6 @@ sns.regplot(x="Price", y="Rating", color = 'darkorange',data=data)
 #Setting the plot title
 plt.title('Rating vs Price[Reg Plot]',size = 20)
 
-#Code ends here
-
-
-# --------------
-
-#Code starts here
 #Finding the length of unique genres
 print( len(data['Genres'].unique()) , "genres")
 
@@ -138,23 +99,12 @@ data['Genres'] = data['Genres'].str.split(';').str[0]
 
 #Grouping Genres and Rating
 gr_mean=data[['Genres', 'Rating']].groupby(['Genres'], as_index=False).mean()
-
 print(gr_mean.describe())
 
 #Sorting the grouped dataframe by Rating
 gr_mean=gr_mean.sort_values('Rating')
-
 print(gr_mean.head(1))
-
 print(gr_mean.tail(1))
-
-
-#Code ends here
-
-
-# --------------
-
-#Code starts here
 
 #Converting the column into datetime format
 data['Last Updated'] = pd.to_datetime(data['Last Updated'])
@@ -170,8 +120,3 @@ sns.regplot(x="Last Updated Days", y="Rating", color = 'lightpink',data=data )
 
 #Setting the title of the plot
 plt.title('Rating vs Last Updated [RegPlot]',size = 20)
-
-
-#Code ends here
-
-
