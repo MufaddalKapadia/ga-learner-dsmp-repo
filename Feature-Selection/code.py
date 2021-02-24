19cd90b0db0f11a16ff5004dda79c15deeedfb0b
Mufaddal Kapadia <31702449+MufaddalKapadia@users.noreply.github.com>
2021-02-24 20:20:23 +0530

    Update code.py

--- Feature-Selection/code.py
+++ Feature-Selection/code.py
@@ -1,10 +1,18 @@
 # --------------
 import pandas as pd
 from sklearn import preprocessing
+import seaborn as sns
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn import cross_validation
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import SelectPercentile
+from sklearn.feature_selection import f_classif
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, precision_score
 
 #path : File path
-
-# Code starts here
 # read the dataset
 dataset = pd.read_csv(path)
 
@@ -17,12 +25,7 @@ dataset.drop(columns = 'Id', inplace=True)
 # check the statistical description
 dataset.describe()
 
-
-# --------------
 # We will visualize all the attributes using Violin Plot - a combination of box and density plots
-import seaborn as sns
-from matplotlib import pyplot as plt
-
 #names of all the attributes 
 cols = dataset.columns
 
@@ -40,15 +43,9 @@ for i in range(0,size):
     sns.violinplot(data=dataset,x=x,y=y[i])  
     plt.show()
 
-
-
-# --------------
-import numpy
 upper_threshold = 0.5
 lower_threshold = -0.5
 
-
-# Code Starts Here
 # create a subset of dataframe with only the first 10 features
 subset_train = dataset.iloc[:, :10]
 
@@ -65,17 +62,6 @@ correlation = data_corr.unstack().sort_values(kind='quicksort')
 # Select the highest correlation pairs using slicing
 corr_var_list = correlation[((correlation>upper_threshold) | (correlation<lower_threshold)) & (correlation!=1)]
 print(corr_var_list)
-
-
-# Code ends here
-
-
-
-
-# --------------
-#Import libraries 
-from sklearn import cross_validation
-from sklearn.preprocessing import StandardScaler
 
 # Identify the unnecessary columns and remove it 
 dataset.drop(columns=['Soil_Type7', 'Soil_Type15'], inplace=True)
@@ -101,12 +87,6 @@ X_test1 = numpy.concatenate((X_test_temp,X_test.iloc[:,10:c-1]),axis=1)
 scaled_features_train_df = pd.DataFrame(X_train1, index=X_train.index, columns=X_train.columns)
 scaled_features_test_df = pd.DataFrame(X_test1, index=X_test.index, columns=X_test.columns)
 
-
-# --------------
-from sklearn.feature_selection import SelectPercentile
-from sklearn.feature_selection import f_classif
-
-
 # Write your solution here:
 skb = SelectPercentile(score_func = f_classif, percentile= 90)
 predictors = skb.fit_transform(X_train1, Y_train)
@@ -122,30 +102,15 @@ top_k_predictors = list(dataframe['Features'][:predictors.shape[1]])
 
 print(top_k_predictors)
 
-
-
-# --------------
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, precision_score
-
 clf = OneVsRestClassifier(LogisticRegression())
 clf1 = OneVsRestClassifier(LogisticRegression())
 
 model_fit_all_features = clf1.fit(X_train, Y_train)
-
 predictions_all_features = model_fit_all_features.predict(X_test)
-
 score_all_features = accuracy_score(Y_test, predictions_all_features)
-
 print(score_all_features)
 
 model_fit_top_features = clf.fit(scaled_features_train_df[top_k_predictors], Y_train)
-
 predictions_top_features = model_fit_top_features.predict(scaled_features_test_df[top_k_predictors])
-
 score_top_features = accuracy_score(Y_test, predictions_top_features)
-
 print(score_top_features)
-
-
