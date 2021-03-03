02c7df29a50add769b8bf4de16a42d9b6d1f0afb
Mufaddal Kapadia <31702449+MufaddalKapadia@users.noreply.github.com>
2021-03-03 20:45:35 +0530

    Update code.py

--- Decision-Trees/code.py
+++ Decision-Trees/code.py
@@ -3,9 +3,19 @@
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import seaborn as sns
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+from io import StringIO
+from sklearn.tree import export_graphviz
+from sklearn import tree
+from sklearn import metrics
+from IPython.display import Image
+import pydotplus
 
-
-# Code starts here
 #Loading of data
 data=pd.read_csv(path)
 
@@ -18,32 +28,12 @@ y=data['paid.back.loan'].copy()
 #Splitting the data into train and test sets
 X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=0.3,random_state=0)
 
-# Code ends here
-
-
-# --------------
-#Importing header files
-import matplotlib.pyplot as plt
-
-# Code starts here
 #Storing value counts of target variable in 'fully_paid'
 fully_paid=y_train.value_counts()
 
 #Plotting bar plot
 plt.bar(fully_paid.index, fully_paid)
 plt.show()
-
-
-# Code ends here
-
-
-# --------------
-#Importing header files
-import numpy as np
-from sklearn.preprocessing import LabelEncoder
-
-
-# Code starts here
 
 #Removing the last character from the values in column
 X_train['int.rate'] = X_train['int.rate'].map(lambda x: str(x)[:-1])
@@ -63,19 +53,8 @@ num_df=X_train.select_dtypes(include=['number']).copy()
 #Storing all the categorical type columns in 'cat_df'
 cat_df=X_train.select_dtypes(include=['object']).copy()
 
-
-# Code ends here
-
-
-# --------------
-#Importing header files
-import seaborn as sns
-
-
-# Code starts here
 #Setting the figure size
 plt.figure(figsize=(20,20))
-
 
 #Storing the columns of 'num_df'
 cols=list(num_df.columns)
@@ -91,11 +70,6 @@ for i in range(9):
     
     #Avoiding subplots overlapping
     fig.tight_layout()    
-# Code ends here
-
-
-# --------------
-# Code starts here
 
 #Storing the columns of 'cat_df'
 cols=list(cat_df.columns)
@@ -114,15 +88,6 @@ for i in range(0,2):
         
         #Avoiding subplots overlapping
         fig.tight_layout()   
-
-# Code ends here
-
-
-# --------------
-#Importing header files
-from sklearn.tree import DecisionTreeClassifier
-
-# Code starts here
 
 #Looping through categorical columns
 for col in cat_df.columns:
@@ -160,17 +125,8 @@ acc=model.score(X_test, y_test)
 #Printing the accuracy
 print(acc)
 
-# Code ends here
-
-
-# --------------
-#Importing header files
-from sklearn.model_selection import GridSearchCV
-
 #Parameter grid
 parameter_grid = {'max_depth': np.arange(3,10), 'min_samples_leaf': range(10,50,10)}
-
-#Code starts here
 
 #Initialising 'Decision Tree' model
 model_2 = DecisionTreeClassifier(random_state=0)
@@ -187,22 +143,6 @@ acc_2 = p_tree.score(X_test, y_test)
 #Printing the accuracy
 print(acc_2)
 
-
-# Code ends here
-
-
-# --------------
-#Importing header files
-
-from io import StringIO
-from sklearn.tree import export_graphviz
-from sklearn import tree
-from sklearn import metrics
-from IPython.display import Image
-import pydotplus
-
-# Code starts here
-
 dot_data = export_graphviz(decision_tree=p_tree.best_estimator_, out_file=None, feature_names=X.columns, filled = True,class_names=['loan_paid_back_yes','loan_paid_back_no'])
 graph_big = pydotplus.graph_from_dot_data(dot_data)
 
@@ -214,7 +154,3 @@ plt.figure(figsize=(20,15))
 plt.imshow(plt.imread(img_path))
 plt.axis('off')
 plt.show() 
-
-# Code ends here
-
-
