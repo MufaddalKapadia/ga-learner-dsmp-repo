0cec6a6fc9a964242339992b200a977fb7043368
Mufaddal Kapadia <31702449+MufaddalKapadia@users.noreply.github.com>
2021-03-08 18:04:58 +0530

    Update code.py

--- Ensemble-Methods/code.py
+++ Ensemble-Methods/code.py
@@ -1,25 +1,36 @@
 # --------------
+import warnings
+warnings.filterwarnings('ignore')
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import VotingClassifier
 import pandas as pd
 
-# Code starts here
+# Read the data
 df = pd.read_csv(path)
+print(df.head())
+
+# Independent variable
 X= df.iloc[ : , :-1].values
+
+# Dependent variable
 y= df.iloc[ : , -1].values
+
+# Split the data
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.3, random_state =4)
+
+# Standardize the data
 scalar = MinMaxScaler()
 scalar.fit(X_train)
 X_train = scalar.transform(X_train)
 X_test = scalar.transform(X_test)
-
-# Code ends here
-
-
-# --------------
-from sklearn.metrics import classification_report
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score
 
 # Initialize the logistic regression
 lr = LogisticRegression()
@@ -33,58 +44,50 @@ y_pred = lr.predict(X_test)
 # roc score
 roc_score = roc_auc_score(y_pred,y_test)
 
+# Initialize decision tree
+dt = DecisionTreeClassifier(random_state = 4)
 
-# --------------
-from sklearn.tree import DecisionTreeClassifier
-
-dt = DecisionTreeClassifier(random_state=4)
-
-# Fit the model 
+# Fit the model on train data
 dt.fit(X_train,y_train)
 
-# Store the predicted values of test data
+# accuracy
+accuracy = dt.score(X_test,y_test)
+
+# Predicted values for test data
 y_pred = dt.predict(X_test)
 
-# roc score
-roc_score = roc_auc_score(y_pred,y_test)
+# ROC score
+roc_score = roc_auc_score(y_test,y_pred)
 
-
-# --------------
-from sklearn.ensemble import RandomForestClassifier
-
-
-# Code strats here
+# Initialize RandomForrest model to variable rfc
 rfc = RandomForestClassifier(random_state=4)
 
-# Fit the model 
+# Fit the model
 rfc.fit(X_train,y_train)
 
 # Store the predicted values of test data
 y_pred = rfc.predict(X_test)
 
+# accuracy
+accuracy = rfc.score(X_test,y_test)
+
 # roc score
-roc_score = roc_auc_score(y_pred,y_test)
+roc_score = roc_auc_score(y_test,y_pred)
 
+# Initialize Bagging Classifier
+bagging_clf = BaggingClassifier(DecisionTreeClassifier(), random_state=0,n_estimators=100,max_samples=100)
 
-# Code ends here
+# Fit the model on training data
+bagging_clf.fit(X_train,y_train)
 
+# Predicted values of X_test
+y_pred = bagging_clf.predict(X_test)
 
-# --------------
-# Import Bagging Classifier
-from sklearn.ensemble import BaggingClassifier
+# accuracy 
+print(bagging_clf.score(X_test,y_test))
 
-
-# Code starts here
-bagging_clf = BaggingClassifier(base_estimator= DecisionTreeClassifier(), n_estimators=100 , max_samples=100, random_state=0)
-bagging_clf.fit(X_train, y_train)
-score_bagging = bagging_clf.score(X_test, y_test)
-
-# Code ends here
-
-
-# --------------
-# Import libraries
-from sklearn.ensemble import VotingClassifier
+# roc_score
+score_bagging = roc_auc_score(y_test,y_pred)
 
 # Various models
 clf_1 = LogisticRegression()
@@ -93,13 +96,12 @@ clf_3 = RandomForestClassifier(random_state=4)
 
 model_list = [('lr',clf_1),('DT',clf_2),('RF',clf_3)]
 
-
 # Code starts here
-voting_clf_hard = VotingClassifier(estimators = model_list, voting = 'hard')
-voting_clf_hard.fit(X_train, y_train)
-hard_voting_score=voting_clf_hard.score(X_test, y_test)
+# Initialize voting classifier
+voting_clf_hard = VotingClassifier(estimators=model_list,voting='hard')
 
+# Fit the model on training data
+voting_clf_hard.fit(X_train,y_train)
 
-# Code ends here
-
-
+# accuracy
+hard_voting_score = voting_clf_hard.score(X_test,y_test)
