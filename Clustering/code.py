fa76e1e50811d41cea4d9bdb3ef5c68bc0811458
Mufaddal Kapadia <31702449+MufaddalKapadia@users.noreply.github.com>
2021-03-17 18:17:11 +0530

    Update code.py

--- Clustering/code.py
+++ Clustering/code.py
@@ -5,43 +5,33 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt 
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 
 # Load Offers
-offers = pd.read_excel(path, sheet_name = 0)
+offers = pd.read_excel(path, sheet_name=0)
 
 # Load Transactions
-transactions = pd.read_excel(path, sheet_name = 1)
+transactions = pd.read_excel(path, sheet_name=1)
 transactions['n'] = 1
+
 # Merge dataframes
 df = pd.merge(transactions, offers)
 
 # Look at the first 5 rows
 print(df.head())
 
-
-# --------------
-# Code starts here
-
 # create pivot table
 matrix = df.pivot_table(index='Customer Last Name', columns='Offer #', values='n')
 
 # replace missing values with 0
-matrix.fillna(0,inplace = True)
+matrix.fillna(0, inplace=True)
 
 # reindex pivot table
 matrix.reset_index(inplace=True)
 
 # display first 5 rows
-print(df.head())
-
-# Code ends here
-
-
-# --------------
-# import packages
-from sklearn.cluster import KMeans
-
-# Code starts here
+print(matrix.head())
 
 # initialize KMeans object
 cluster = KMeans(n_clusters=5, init='k-means++', max_iter=300, n_init=10, random_state=0)
@@ -50,32 +40,19 @@ cluster = KMeans(n_clusters=5, init='k-means++', max_iter=300, n_init=10, random
 matrix['cluster'] = cluster.fit_predict(matrix[matrix.columns[1:]])
 print(matrix.head())
 
-# Code ends here
-
-
-# --------------
-# import packages
-from sklearn.decomposition import PCA
-
-# Code starts here
-
 # initialize pca object with 2 components
 pca = PCA(n_components=2, random_state=0)
 
 # create 'x' and 'y' columns donoting observation locations in decomposed form
 matrix['x'] = pca.fit_transform(matrix[matrix.columns[1:]])[:,0]
 matrix['y'] = pca.fit_transform(matrix[matrix.columns[1:]])[:,1]
+
 # dataframe to visualize clusters by customer names
 clusters = matrix.iloc[:,[0,33,34,35]]
 
 # visualize clusters
 clusters.plot.scatter(x='x', y='y', c='cluster', colormap='viridis')
-
-# Code ends here
-
-
-# --------------
-# Code starts here
+plt.show()
 
 # merge 'clusters' and 'transactions'
 data = pd.merge(clusters, transactions)
@@ -108,11 +85,6 @@ cluster_champagne = max(champagne, key=champagne.get)
 print(cluster_champagne)
 #     print('='*50)
 
-
-
-# --------------
-# Code starts here
-
 # empty dictionary
 discount = {}
 
@@ -128,6 +100,3 @@ for val in data.cluster.unique():
 # cluster with maximum average discount
 cluster_discount = max(discount, key=discount.get)
 print(cluster_discount)
-# Code ends here
-
-
