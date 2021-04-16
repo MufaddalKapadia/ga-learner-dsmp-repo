12453ed5753e6d8779cd47c041eeb150e32ec5bf
Mufaddal Kapadia <31702449+MufaddalKapadia@users.noreply.github.com>
2021-04-16 16:55:32 +0530

    Update code.py

--- Numpy_project_1/code.py
+++ Numpy_project_1/code.py
@@ -5,24 +5,18 @@ import numpy as np
 #New record
 new_record=[[50,  9,  4,  1,  0,  0, 40,  0]]
 
-#Code starts here
-
 #Loading data file and saving it into a new numpy array 
 data = np.genfromtxt(path, delimiter=",", skip_header=1)
-
 print(data.shape)
 
 #Concatenating the new record to the existing numpy array
 census=np.concatenate((data, new_record),axis = 0)
-
 print(census.shape)
 
-
-# --------------
-#Code starts here
 #Subsetting the array to include only 'Age' column
 age=census[:,0]
 print(age)
+
 #Finding the max value of age
 max_age=age.max()
 print("Max Age= ",max_age)
@@ -39,15 +33,11 @@ print("Age Average= ", age_mean)
 age_std=age.std()
 print("Age Standard Deviation= ",age_std)
 
-
-# --------------
-#Code starts here
 race_0=census[census[:,2]==0]
 race_1=census[census[:,2]==1]
 race_2=census[census[:,2]==2]
 race_3=census[census[:,2]==3]
 race_4=census[census[:,2]==4]
-
 
 #Finding the length of the above created subsets
 len_0=len(race_0)
@@ -69,9 +59,6 @@ race_list=[len_0, len_1,len_2, len_3, len_4]
 #Storing the race with minimum length into a variable 
 minority_race=race_list.index(min(race_list))
 
-
-# --------------
-#Code starts here
 senior_citizens=census[census[:,0]>60]
 
 #Calculating the sum of all the values of array
@@ -86,11 +73,6 @@ avg_working_hours=working_hours_sum/senior_citizens_len
 #Printing the average working hours
 print((avg_working_hours))
 
-#Code ends here
-
-
-# --------------
-#Code starts here
 #Creating an array based on 'education' column
 high=census[census[:,1]>10]
 
@@ -108,5 +90,3 @@ avg_pay_low=low[:,7].mean()
 
 #Printing the average pay
 print(avg_pay_low)
-
-
